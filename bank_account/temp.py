77343754a38ee63a97bc6fabadbb0d87b43ea959
sara <sarrarostami77@gmail.com>
2024-08-16 22:13:38 +0330

    new commit

--- bank_account/temp.py
+++ bank_account/temp.py
@@ -5,24 +5,34 @@ from .models import BankAccount
 import random
 from decimal import Decimal
 
-def create_fake_bankaccounts():
+def create_fake_bankaccounts(total_records, batch_size):
     fake = Faker()
     persons = list(Person.objects.all())
-    total_records = 2000
-    batch_size = 100
+
+    existing_account = set(BankAccount.objects.values_list('account_number', flat=True))
+
+    def generate_unique():
+        while True:
+            account_number = fake.unique.numerify(text='################')
+            if account_number not in existing_account:
+                existing_account.add(account_number)
+                return account_number
+
     bank_accounts = (
         BankAccount(
             owner=random.choice(persons),
             balance=round(random.uniform(100.00, 100000.00), 2),
-            account_number=fake.unique.numerify(text='################')
+            account_number=generate_unique()
         )
-        for _ in range(2000)
+        for _ in range(total_records)
     )
+
     while True:
-        batch = list(islice(bank_accounts,batch_size))
+        batch = list(islice(bank_accounts, batch_size))
         if not batch:
             break
         BankAccount.objects.bulk_create(batch, batch_size)
+
     print(f"Total of {total_records} records created in batches of {batch_size}.")
 
 
--- person/temp.py
+++ person/temp.py
@@ -1,25 +1,65 @@
 from faker import Faker
 from .models import Person
 from itertools import islice
+from django.db import transaction
+from django.core.exceptions import ValidationError
+from bank_account.models import BankAccount
+import random
 
-
-def create_fake_persons():
+def create_fake_persons(total_records, batch_size):
     fake = Faker()
+    existing_codes = set(Person.objects.values_list('national_code', flat=True))
 
-    total_records = 500
-    batch_size = 100
+    def generate_unique():
+        while True:
+            code = fake.unique.numerify(text='##########')
+            if code not in existing_codes:
+                existing_codes.add(code)
+                return code
 
     persons = (
         Person(
-            first_name = fake.first_name(),
-            last_name = fake.last_name(),
-            national_code = fake.unique.numerify(text='##########')
+            first_name=fake.first_name(),
+            last_name=fake.last_name(),
+            national_code=generate_unique()
         )
-        for _ in range(500)
+        for _ in range(total_records)
     )
+
     while True:
-        batch = list(islice(persons,batch_size))
+        batch = list(islice(persons, batch_size))
         if not batch:
             break
         Person.objects.bulk_create(batch, batch_size)
+
     print(f"Total of {total_records} records created in batches of {batch_size}.")
+
+
+def transfer_value(from_account_number, from_owner_firstname,from_owner_lastname, to_account_number, to_owner_firstname,to_owner_lastname, value):
+
+    try:
+        with transaction.atomic():
+            from_account = BankAccount.objects.select_for_update().get(
+                account_number=from_account_number,
+                owner__first_name=from_owner_firstname,
+                owner__last_name=from_owner_lastname
+            )
+            to_account = BankAccount.objects.get(
+                account_number=to_account_number,
+                owner__first_name=to_owner_firstname,
+                owner__last_name=to_owner_lastname
+            )
+
+
+            if from_account.balance < value:
+                raise ValidationError("not allowed.")
+
+            from_account.balance -= value
+            from_account.save()
+            to_account.balance += value
+            to_account.save()
+
+            print(f"Transferred {value} from account {from_account_number} to account {to_account_number}.")
+
+    except BankAccount.DoesNotExist:
+        raise ValidationError("the accounts do not exist.")
